07bb1a67b73e16b08ce85415f791e314dba859fc
Yogesh patel <yogeshtech79@gmail.com>
2023-12-05 23:01:42 +0530

    change

--- myfirstapp/src/Home.jsx
+++ myfirstapp/src/Home.jsx
@@ -6,7 +6,9 @@ export function Home(props){           //props become obeject access from app.js
         <div>
             <center>
             {/* this is props value taken from app.js */}
-            <h1>{props.headerText}</h1>      
+            <h1>{props.headerText}</h1>    
+            <h1>{props.headerText}</h1>   
+            <h1>{props.headerText}</h1> 
             <p>{props.description}</p>
             <hr />
             </center>
