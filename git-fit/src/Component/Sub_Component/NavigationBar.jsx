a7ce1dfdfeba0678a2ec90e5dbf7219b3e290556
Yogesh patel <yogeshtech79@gmail.com>
2023-12-07 09:19:45 +0530

    change

--- git-fit/src/Component/Sub_Component/NavigationBar.jsx
+++ git-fit/src/Component/Sub_Component/NavigationBar.jsx
@@ -1,31 +1,33 @@
-import { Container, Nav, Navbar } from "react-bootstrap";
+import { Alert, Container, Nav, Navbar } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
 
 export function NavigationBar() {
     return (
 
-        
-        <Navbar expand="lg" bg="dark" data-bs-theme="dark">
-            <Container>
-                <Navbar.Brand href="#home">Go-Fit</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                      <LinkContainer to="/">
-                            <Nav.Link>Home</Nav.Link>
-                        </LinkContainer>
-                        <LinkContainer to="/aboutus">
-                            <Nav.Link>About Us</Nav.Link>
-                        </LinkContainer>
-                        <LinkContainer to="/programs">
-                            <Nav.Link>Programs</Nav.Link>
-                        </LinkContainer>
-                        <LinkContainer to="/contactus">
-                            <Nav.Link>Contact Us</Nav.Link>
-                        </LinkContainer>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
+
+        <Container>
+            <Navbar expand="lg" bg="dark" data-bs-theme="dark">
+                <Container>
+                    <Navbar.Brand href="#home">Go-Fit</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <LinkContainer to="/">
+                                <Nav.Link>Home</Nav.Link>
+                            </LinkContainer>
+                            <LinkContainer to="/aboutus">
+                                <Nav.Link>About Us</Nav.Link>
+                            </LinkContainer>
+                            <LinkContainer to="/programs">
+                                <Nav.Link>Programs</Nav.Link>
+                            </LinkContainer>
+                            <LinkContainer to="/contactus">
+                                <Nav.Link>Contact Us</Nav.Link>
+                            </LinkContainer>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </Container>
     );
 }
