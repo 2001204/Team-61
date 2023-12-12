5818eb6e3f29ec781e02e1ed00f5308bce3f3afe
Dipak Wani <dipakwani204@gmail.com>
2023-12-12 08:18:25 +0530

    Project video and photos uploaded

--- server/constants.js
+++
@@ -1,5 +0,0 @@
-export const ERROR_MESSAGE="Something went wrong";
-export const INSERT_SUCCESS="Customer inserted successfully";
-export const CUSTOMER_NOT_FOUND="Customer not found";
-export const UPDATE_SUCCESS="Customer Updated";
-export const DELETE_SUCCESS="Customer Delete";
